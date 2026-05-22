a5c58eb48f0d1a8aed3f6d7542a051d0171fefc2
Ononobi Praise <ononobip@gmail.com>
2026-05-22 18:15:46 +0100

    fixed text layout

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -142,7 +142,7 @@ const TransactionsPage = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-2 md:grid-cols-4 md:gap-4 gap-2 py-4 mb-8">
+      <div className="grid grid-cols-1 md:grid-cols-4 md:gap-4 gap-2 py-4 mb-8">
         <StatCard
           themeColor="green"
           icon={<RevenueIcon color="#04802E" />}
