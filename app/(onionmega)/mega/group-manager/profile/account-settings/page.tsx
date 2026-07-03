7d5ae6ec3630ed3f4fe84cb96c29c0e48ca87d4b
Ononobi Praise <ononobip@gmail.com>
2026-07-03 08:12:20 +0100

    feat: create group manager dashboard

---
+++ app/(onionmega)/mega/group-manager/profile/account-settings/page.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ProfileLayout from "../Shell";
+import { SecurityInformation } from "@/components/account-settings/security-information";
+import { TwoFactor } from "@/components/account-settings/two-factor";
+import { Billing } from "@/components/account-settings/billing";
+import { Notification } from "@/components/account-settings/notification";
+
+const page = () => {
+  return (
+    <ProfileLayout
+      active="account-settings"
+      heading="Account Settings"
+      subheading="Security, billing, and integrations"
+    >
+      <div className="space-y-10">
+        <SecurityInformation />
+        <TwoFactor />
+        <Notification />
+        <Billing />
+      </div>
+    </ProfileLayout>
+  );
+};
+
+export default page;
---
+++ app/(onionmega)/mega/group-manager/staff/leave-request/page.tsx
@@ -0,0 +1,12 @@
+import LeaveRequestPage from '@/components/staff-page/leave-request'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <LeaveRequestPage />
+    </div>
+  )
+}
+
+export default page
